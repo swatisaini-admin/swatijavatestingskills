99641c661a41cb89d42bc48f9e889792d6307e70
swati saini <swatisaini12@yahoo.co.in>
2020-01-21 13:53:49 +0530

    added new files

---
+++ src/studyjava/EmployeeDetails.java
@@ -0,0 +1,21 @@
+package studyjava;
+
+public class EmployeeDetails {
+int EmpAge;
+char name;
+int phoneno;
+
+	void address()
+	{
+	System.out.println("phoneno");
+}
+void identification()
+{
+	System.out.println("name");
+}
+
+	void level()
+	{
+		System.out.println("EmpAge");
+	}
+}
---
+++ src/studyjava/EmployeeSkills.java
@@ -0,0 +1,14 @@
+package studyjava;
+
+public class EmployeeSkills {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EmployeeDetails E1=new EmployeeDetails();
+        E1.address();
+	EmployeeDetails E2=new EmployeeDetails();
+	E2.identification();
+	EmployeeDetails E3=new EmployeeDetails();
+	E3.level();
+	}
+}
